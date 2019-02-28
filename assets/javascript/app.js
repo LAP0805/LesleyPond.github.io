1c799a0228e898b9f21fa81d2ea9dacdca885446
Janxxy <lesley.pond@gmail.com>
2019-02-28 01:57:52 -0500

    initial

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+
+ $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+  $(".portfolio").on("click",function(){
+    window.location.href = "portfolio.html";
+  })
