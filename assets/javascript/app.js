1147aaac0ff90673aae299a5a318b6dcb4619773
Janxxy <lesley.pond@gmail.com>
2020-03-29 10:02:07 -0400

    added later projects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,10 @@
-
- $(document).ready(function(){
-    $('.parallax').parallax();
-    $(".dropdown-trigger").dropdown();
-  });
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
-  $(".portfolio").on("click",function(){
-    window.location.href = "portfolio.html";
-  })
+$(document).ready(function () {
+  $('.parallax').parallax();
+  $(".dropdown-trigger").dropdown();
+});
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+$(".portfolio").on("click", function () {
+  window.location.href = "portfolio.html";
+})
